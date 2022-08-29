a63c82baeab9afd12c3756eb8d73abb1285ee8e8
JintekiWarrior <jintekiwarrior@gmail.com>
2022-08-29 18:45:26 -0400

    Initial commit

---
+++ wanderersQuest/src/main/java/com/moeGames/wanderersQuest/WanderersQuestApplication.java
@@ -0,0 +1,13 @@
+package com.moeGames.wanderersQuest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WanderersQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WanderersQuestApplication.class, args);
+	}
+
+}
---
+++ wanderersQuest/src/test/java/com/moeGames/wanderersQuest/WanderersQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moeGames.wanderersQuest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WanderersQuestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
