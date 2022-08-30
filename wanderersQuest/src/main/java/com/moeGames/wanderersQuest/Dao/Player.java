bd8c09a8a29d803ce8f0055d9e38ee8df728cc57
JintekiWarrior <jintekiwarrior@gmail.com>
2022-08-30 13:43:18 -0400

    reconfigure application properties add schema to player entity

--- wanderersQuest/src/main/java/com/moeGames/wanderersQuest/Dao/Player.java
+++ wanderersQuest/src/main/java/com/moeGames/wanderersQuest/Dao/Player.java
@@ -1,4 +1,70 @@
 package com.moeGames.wanderersQuest.Dao;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tbl_Players")
 public class Player {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+
+    // player stats
+    private int health;
+    private int stamina;
+    private int mana;
+
+    public Player() {
+    }
+
+    public Player(int id, String name, int health, int stamina, int mana) {
+        this.id = id;
+        this.name = name;
+        this.health = health;
+        this.stamina = stamina;
+        this.mana = mana;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getStamina() {
+        return stamina;
+    }
+
+    public void setStamina(int stamina) {
+        this.stamina = stamina;
+    }
+
+    public int getMana() {
+        return mana;
+    }
+
+    public void setMana(int mana) {
+        this.mana = mana;
+    }
 }
