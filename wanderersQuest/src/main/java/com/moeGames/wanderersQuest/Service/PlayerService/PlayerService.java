2fdded5d2871bcd980925f240ee0c02c2eb9de13
JintekiWarrior <jintekiwarrior@gmail.com>
2022-09-06 01:22:02 -0400

    -Add playerserviceimpl methods -Fix bug with playerDao and Entity

--- wanderersQuest/src/main/java/com/moeGames/wanderersQuest/Service/PlayerService/PlayerService.java
+++ wanderersQuest/src/main/java/com/moeGames/wanderersQuest/Service/PlayerService/PlayerService.java
@@ -1,13 +1,13 @@
 package com.moeGames.wanderersQuest.Service.PlayerService;
 
-import com.moeGames.wanderersQuest.Dao.PlayerDao;
+import com.moeGames.wanderersQuest.Entity.Player;
 
 import java.util.List;
 
 public interface PlayerService {
-    List<PlayerDao> getAllPlayers();
-    PlayerDao getPlayerById(int playerId);
-    PlayerDao addPlayer(PlayerDao player);
-    PlayerDao updatePlayer(PlayerDao player);
+    List<Player> getAllPlayers();
+    Player getPlayerById(int playerId);
+    Player addPlayer(Player player);
+    Player updatePlayer(Player player);
     String deletePlayerById(int playerId);
 }
--- wanderersQuest/src/main/java/com/moeGames/wanderersQuest/Service/PlayerService/PlayerServiceImpl.java
+++ wanderersQuest/src/main/java/com/moeGames/wanderersQuest/Service/PlayerService/PlayerServiceImpl.java
@@ -1,10 +1,12 @@
 package com.moeGames.wanderersQuest.Service.PlayerService;
 
 import com.moeGames.wanderersQuest.Dao.PlayerDao;
+import com.moeGames.wanderersQuest.Entity.Player;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PlayerServiceImpl implements PlayerService
@@ -13,27 +15,38 @@ public class PlayerServiceImpl implements PlayerService
     private PlayerDao playerDao;
 
     @Override
-    public List<PlayerDao> getAllPlayers() {
+    public List<Player> getAllPlayers() {
         return this.playerDao.findAll();
     }
 
     @Override
-    public PlayerDao getPlayerById(int playerId) {
-        return null;
+    public Player getPlayerById(int playerId) {
+        Optional <Player> p = this.playerDao.findById(playerId);
+        Player play = null;
+        if (p.isPresent())
+        {
+            play = p.get();
+        } else
+        {
+            throw new RuntimeException("Player Id was not found. " + playerId);
+        }
+
+        return play;
     }
 
     @Override
-    public PlayerDao addPlayer(PlayerDao player) {
-        return null;
+    public Player addPlayer(Player player) {
+        return this.playerDao.save(player);
     }
 
     @Override
-    public PlayerDao updatePlayer(PlayerDao player) {
-        return null;
+    public Player updatePlayer(Player player) {
+        return this.playerDao.save(player);
     }
 
     @Override
     public String deletePlayerById(int playerId) {
-        return null;
+        this.playerDao.deleteById(playerId);
+        return "Deleted Player Successfully";
     }
 }
