4c7c4e3ea70b73326f0bb0b9e9270bff1ca06943
JintekiWarrior <jintekiwarrior@gmail.com>
2022-09-06 00:40:53 -0400

    Add PlayerService interface

---
+++ wanderersQuest/src/main/java/com/moeGames/wanderersQuest/Service/PlayerService/PlayerService.java
@@ -0,0 +1,13 @@
+package com.moeGames.wanderersQuest.Service.PlayerService;
+
+import com.moeGames.wanderersQuest.Dao.Player;
+
+import java.util.List;
+
+public interface PlayerService {
+    List<Player> getAllPlayers();
+    Player getPlayerById(int playerId);
+    Player addPlayer(Player player);
+    Player updatePlayer(Player player);
+    String deletePlayerById(int playerId);
+}
